4b370317e5c6f9bc599fdd4ae0e2a927537ed244
Zakaria Babahmida <zbabahmi@c1r8p4.1337.ma>
2023-12-23 02:51:32 +0100

    new

---
+++ ex01/fixed.cpp
@@ -0,0 +1,78 @@
+#include "fixed.hpp"
+
+static	float ft_pow(float base, int exp)
+{
+	float	result;
+
+	if (!exp)
+		return (1);
+	if (exp < 0)
+	{
+		base = 1 / base;
+		exp *= -1;
+	}
+	result = base;
+	while (--exp)
+		result *= base;
+	return (result);
+}
+
+const int	Fixed::_frac = 8;
+
+Fixed::Fixed(void): _value(0)
+{
+	std::cout << "Fixed object created with default constructor" << std::endl;
+}
+
+Fixed::Fixed(const int value): _value(value * ft_pow(2, this->_frac))
+{
+	std::cout << "Fixed object created with int constructor" << std::endl;
+}
+
+Fixed::Fixed(const float value): _value(value * ft_pow(2, this->_frac))
+{
+	std::cout << "Fixed object created with float constructor" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Fixed object destroyed" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & copy)
+{
+	std::cout << "Fixed object copied" << std::endl;
+	*this = copy;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return (this->_value * ft_pow(2, -this->_frac));
+}
+
+int	Fixed::toInt(void) const
+{
+	return (this->_value * ft_pow(2, -this->_frac));
+}
+
+Fixed	&Fixed::operator=(Fixed const &copy)
+{
+	std::cout << "Assignment operator called" << std::endl;
+	this->_value = copy.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	return (this->_value);
+}
+
+void	Fixed::setRawBits(const int raw)
+{
+	this->_value = raw;
+}
+
+std::ostream	&operator<<(std::ostream &str, Fixed const &fixed_nbr)
+{
+	return (str << fixed_nbr.toFloat());
+}
---
+++ ex01/fixed.hpp
@@ -0,0 +1,28 @@
+#ifndef FIXED_HPP
+
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class	Fixed
+{
+	private:
+		int					_value;
+		static const int	_frac;
+	public:
+		Fixed(void);
+		Fixed(const int value);
+		Fixed(const float value);
+		~Fixed(void);
+		Fixed(Fixed const &copy);
+		Fixed	&operator=(Fixed const &copy);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+};
+
+std::ostream	&operator<<(std::ostream &str, Fixed const &fixed_nbr);
+
+#endif
